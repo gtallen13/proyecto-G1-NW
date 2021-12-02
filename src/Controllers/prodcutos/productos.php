fddab184d1bb58aa6f669cf76f23745c3862c9ab
JonathanA03 <alemanjl.ix@gmail.com>
2021-12-01 19:45:23 -0600

    Controlador Productos

---
+++ src/Controllers/prodcutos/productos.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Controllers\productos;
+
+use Controllers\PublicController;
+
+/**
+ * Listado del WW para administrar las Partituras que estaran desplegadas en
+ * el catálogo.
+ *
+ * @category Public
+ * @package  Controllers
+ * @author   Orlando J Betancourth <orlando.betancourth@gmail.com>
+ * @license  MIT http://
+ * @link     http://
+ */
+class Prodcutos extends PublicController
+{
+    /**
+     * Ejecuta el Controlador de Scores
+     *
+     * @return void
+     */
+    public function run() :void
+    {
+        $viewData = array();
+        \Views\Renderer::render("productos/productos", $viewData);
+    }
+}
